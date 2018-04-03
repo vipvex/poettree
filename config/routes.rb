54e502a78c7c11d8aa39f7b51254beacdd3a8ef8
vipvex <aleksander.battle@gmail.com>
2018-04-03 19:29:34 +0000

    Add static pages and gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'static_pages#home'
+  
+  get 'about_us', to: 'static_pages#about_us'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
