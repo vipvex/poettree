70804aac5c52959efa5fab072ecba8f4a61da999
vipvex <aleksander.battle@gmail.com>
2018-04-15 00:33:50 +0000

    Dashboard front-end uses real data and redirects correctly

--- app/models/lesson_group.rb
+++ app/models/lesson_group.rb
@@ -1,3 +1,16 @@
 class LessonGroup < ApplicationRecord
-  has_many :lessons    
+  has_many :lessons
+  
+  def completed_lessons_by(user)
+    user.lesson_completeds.where(lesson_id: lessons.pluck(:id))
+  end
+  
+  def locked_for?(user)
+    previous_group = LessonGroup.find_by(order: self.order - 1)
+    
+    return false if previous_group == nil
+    
+    return true if previous_group.completed_lessons_by(user).count < previous_group.lessons.count
+  end
+  
 end
